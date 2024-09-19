adb9b98d344eaa19697673aaa6e0deeba9fc5776
Gabriela Dodson <dodsongabi@gmail.com>
2024-09-18 23:38:03 -0400

    Initialize employees with shift schedules

---
+++ cc_6.js
@@ -0,0 +1,39 @@
+//Create an Employees Array of Employee Objects
+const employees = [
+    {
+        name: "Peter Griffin",
+        shifts:[
+            {day: "Monday", hours: 8},
+            {day: "Tuesday", hours: 7},
+            {day: "Wednesday", hours:7.5},
+            {day: "Thursday", hours:8},
+            {day: "Friday", hours:6}
+        ]
+    },
+    {
+        name: "Lois Griffin",
+        shifts:[
+            {day: "Tuesday", hours:6},
+            {day:"Thursday", hours:8},
+            {day: "Friday", hours:10}
+        ]
+    },
+    {
+        name: "Glen Quagmire",
+        shifts:[
+            {day: "Monday", hours:9},
+            {day:"Tuesday", hours:10},
+            {day: "Wednesday", hours: 9.5},
+            {day: "Thursday", hours:8}
+        ]
+    },
+    {
+        name:"Cleveland Brown",
+        shifts:[
+            {day:"Thursday", hours:8},
+            {day:"Friday", hours:7.5},
+            {day:"Saturday", hours:8},
+            {day:"Sunday", hours:9}
+        ]
+    }
+];
